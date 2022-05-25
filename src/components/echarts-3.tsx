e919b9935dfe1534dba76169c5ad65a81004dabf
hisoka1638 <1399952343@qq.com>
2022-05-26 00:34:16 +0800

    完成Echart3

---
+++ src/components/echarts-3.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef } from "react"
+import * as echarts from 'echarts';
+const fx=(n)=>{return  n/ 2420 * (window as any).pagewidth}
+export const Echart3 =()=>{
+ const divref =  useRef(null)
+ useEffect(()=>{
+    var myChart = echarts.init(divref.current);
+    myChart.setOption({
+        legend: {
+            data: ['盗窃', '诈骗', '抢劫', '打人', '斗殴','打架'],
+            bottom: fx(6),
+            itemWidth: fx(30),
+            itemHeight: fx(15),
+            textStyle:{
+                color: 'white',
+                fontSize:fx(16)
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '9%',
+            bottom: '20%',
+            top:'10%',
+            containLabel: true
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
+            splitLine: {show: true, lineStyle: {color: '#073E78'}},
+            axisTick: {show: false},
+            axisLine: {show: false},
+            axisLabel:{
+                fontSize:fx(12)
+            },
+          },
+          yAxis: {
+            type: 'value',
+            splitLine: {lineStyle: {color: '#073E78'}},
+            axisLabel: {
+                fontSize:fx(12),
+                formatter(val) {
+                return val * 100 + '%';
+              }
+            }
+          },
+          series: [
+            {
+              name: '盗窃',
+              type: 'line',
+              data: [0.1,0.2,0.3,0.4,0.5,0.6,0.7]
+            },
+            {
+              name: '诈骗',
+              type: 'line',
+              data: [0.15, 0.36, 0.24,0.27,0.45,0.54,0.12]
+            },
+            {
+              name: '抢劫',
+              type: 'line',
+              data: [0.15, 0.36, 0.24,0.27,0.25,0.54,0.12]
+            },
+            {
+              name: '打人',
+              type: 'line',
+              data: [0.15, 0.36, 0.74,0.27,0.45,0.54,0.12]
+            },
+            {
+              name: '斗殴',
+              type: 'line',
+              data: [0.15, 0.96, 0.34,0.27,0.45,0.54,0.12]
+            },
+            {
+              name: '打架',
+              type: 'line',
+              data: [0.19, 0.36, 0.24,0.27,0.45,0.54,0.12]
+              },
+          ].map(obj=>({
+            ...obj,
+            symbol: 'circle',
+            symbolSize: fx(12),
+            lineStyle: {width: fx(2)}
+          }))
+        })
+    },[]);
+  return(
+      <div className="趋势分析 border">
+        <h2>发案趋势分析</h2>
+        <div ref={divref} className='echart'></div>
+      </div>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import './home.scss';
 import imagehd from '../images/header.png'
 import {Echart1} from '../components/echarts-1'
 import {Echart2} from '../components/echarts-2'
+import {Echart3} from '../components/echarts-3'
 
 export const Home = () => {
   return (
@@ -10,10 +11,12 @@ export const Home = () => {
       <header style={{backgroundImage: `url(${imagehd})`}}></header>
       <main>
       <section className='section1'>
-      <Echart1 />
-      <Echart2 />
+      <Echart1/>
+      <Echart2/>
       </section>
-      <section className='section2 border'></section>
+      <section className='section2'>
+      <Echart3/>
+      </section>
       <section className='section3 border'></section>
       <section className='section4 border'></section>
       <section className='section5 border'></section>
